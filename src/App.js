80ce44718a20c36ed0284d01cd51b2b51fb434da
Tim Liu <timothytcliu@gmail.com>
2020-10-20 16:06:10 -0700

    chore: cleaned up comments

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-// import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.css';
-// import Contact from './components/contact';
 import Hero from './components/hero';
 import Projects from './components/projects';
 import './App.css';
--- src/components/hero.js
+++ src/components/hero.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Jumbotron, Container, Row, Col } from 'reactstrap';
-// import { FaLinkedin, FaGithub } from 'react-icons/fa';
-// import { FiMail } from 'react-icons/fi';
 import Contacts from './contact';
 import '../App.css';
 
@@ -22,27 +20,6 @@ const Hero = () => {
             </Col>
           </Row>
           <Contacts />
-          {/* <Row
-            style={{
-              margin: '0',
-              padding: '0',
-            }}
-          >
-            <Col>
-              <h2>
-                <a
-                  href="https://www.linkedin.com/in/timothy-liu-784990138/"
-                  target="_blank"
-                >
-                  <FaLinkedin color="#0e76a8" />
-                </a>{' '}
-                <a href="https://github.com/timothytcliu" target="_blank">
-                  <FaGithub color="white" />
-                </a>{' '}
-                <FiMail color="#D44638" />
-              </h2>
-            </Col>
-          </Row> */}
         </Container>
       </Jumbotron>
     </>
