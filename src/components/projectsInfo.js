5b251d9bd86ef63fa4da43b0fcea0ba94f683665
Tim Liu <timothytcliu@gmail.com>
2020-10-20 15:44:31 -0700

    feat: added more projects and images

--- src/components/projectsInfo.js
+++ src/components/projectsInfo.js
@@ -5,11 +5,22 @@
 
 import bootstrapNucamp from '../img/bootstrap-nucamp-project.png';
 import reactNucamp from '../img/react-nucampsite-project.png';
+import nativeNucamp from '../img/native-nucampsite.png';
+import youtubeParty from '../img/youtube-party.png';
 
 export const projectsInfo = [
   {
+    name: 'YouTube Party',
+    technologies:
+      'Technologies: React.js, Javascript, Redux, React-Router, Bootstrap',
+    github: 'https://github.com/timothytcliu/YouTube-Party-React',
+    website: 'https://timothytcliu.github.io/YouTube-Party-React/',
+    image: youtubeParty,
+    alt: 'youtube-party-app',
+  },
+  {
     name: 'Nu Camping',
-    technologies: 'Technologies: HTML, Bootstrap 4',
+    technologies: 'Technologies: Bootstrap 4, HTML, CSS',
     github: 'https://github.com/timothytcliu/Bootstrap-Nucamp-project',
     website:
       'https://timothytcliu.github.io/Bootstrap-Nucamp-project/index.html',
@@ -19,26 +30,18 @@ export const projectsInfo = [
   {
     name: 'React Nu Camping',
     technologies:
-      'Technologies: React, Redux, Redux Router, Bootstrap/Reactstrap  ',
+      'Technologies: React.js, Javascript, Redux, React-Router, Bootstrap',
     github: 'https://github.com/timothytcliu/React-nucampsite',
     website: 'https://timothytcliu.github.io/React-nucampsite/',
     image: reactNucamp,
     alt: 'react-nucamp-project',
   },
-  /*{
-    name: '',
-    technologies: 'Technologies: ',
-    github: '',
-    website: '',
-    image: '',
-    alt: '',
-  },
   {
-    name: '',
-    technologies: 'Technologies: ',
-    github: '',
-    website: '',
-    image: '',
-    alt: '',
-  }, */
+    name: 'Native Nucampsite',
+    technologies: 'Technologies: React-Native, Expo, Redux',
+    github: 'https://github.com/timothytcliu/React-Native-nucamp-project',
+    website: '#',
+    image: nativeNucamp,
+    alt: 'react-native-nucamp',
+  },
 ];
